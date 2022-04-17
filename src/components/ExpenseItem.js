bd74a3977d3236ad6ad3d63e2c3882e54ab4fdba
AbhinavS96 <abhinavsidharth@gmail.com>
2022-04-18 00:02:04 +0530

    set up props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,13 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  const expenseTitle = "title";
-  const expenseAmount = "cost";
-  const expenseDate = new Date(2022, 3, 17);
-
+function ExpenseItem(props) {
+  const { title, amount, date } = props;
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString().split("T")[0]}</div>
+      <div>{date.toISOString().split("T")[0]}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">{expenseAmount}</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">{amount}</div>
       </div>
     </div>
   );
