24291d21b8adbc7fa3a98f32e3ecef12b9b7838e
AbhinavS96 <abhinavsidharth@gmail.com>
2022-04-17 15:39:37 +0530

    worked on expense component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,19 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
-    return <h2>Expense</h2>
+  const expenseTitle = "title";
+  const expenseAmount = "cost";
+  const expenseDate = new Date(2022, 3, 17);
+
+  return (
+    <div className="expense-item">
+      <div>{expenseDate.toISOString().split("T")[0]}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">{expenseAmount}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
