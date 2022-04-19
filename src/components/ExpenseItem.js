1cb0290f6ef4b7980d091a60b5220182bc0a33a0
AbhinavS96 <abhinavsidharth@gmail.com>
2022-04-19 22:53:53 +0530

    set up date

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -4,7 +4,11 @@ function ExpenseItem(props) {
   const { title, amount, date } = props;
   return (
     <div className="expense-item">
-      <div>{date.toISOString().split("T")[0]}</div>
+      <div>
+        <div>{date.toLocaleString("en-US", { month: "long" })}</div>
+        <div>{date.toLocaleString("en-US", { day: "2-digit" })}</div>
+        <div>{date.getFullYear()}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">{amount}</div>
