94b966e68a37fa941675fbb6d80beee963489e7e
AbhinavS96 <abhinavsidharth@gmail.com>
2022-04-27 23:13:07 +0530

    split expense component

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,18 @@
+import "./ExpenseDate.css";
+
+function ExpenseDate(props) {
+  const { date } = props;
+  return (
+    <div className="expense-date">
+      <div className="expense-date__month">
+        {date.toLocaleString("en-US", { month: "long" })}
+      </div>
+      <div className="expense-date__year">{date.getFullYear()}</div>
+      <div className="expense-date__day">
+        {date.toLocaleString("en-US", { day: "2-digit" })}
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseDate;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,11 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
   const { title, amount, date } = props;
   return (
     <div className="expense-item">
-      <div>
-        <div>{date.toLocaleString("en-US", { month: "long" })}</div>
-        <div>{date.toLocaleString("en-US", { day: "2-digit" })}</div>
-        <div>{date.getFullYear()}</div>
-      </div>
+      <ExpenseDate date={date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">{amount}</div>
