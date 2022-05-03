db6f0a87570f0ab8c9f5395b39f6d662587888c6
AbhinavS96 <abhinavsidharth@gmail.com>
2022-05-03 14:14:25 +0530

    initial implementation of filter componenet

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,23 +1,52 @@
+import { useState } from "react";
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "./Card";
+import ExpensesFilter from "./ExpensesFilter";
 
 function Expenses(props) {
   const { expenses } = props;
-  return (
-    <Card className="expenses">
-      {expenses.map((expense) => {
-        return (
-          <ExpenseItem
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-            key={expense.id}
-          />
-        );
-      })}
-    </Card>
-  );
+  const [year, setYear] = useState("");
+
+  function filterHandler(year) {
+    setYear(year);
+  }
+
+  if (year) {
+    return (
+      <Card className="expenses">
+        <ExpensesFilter onFilter={filterHandler} />
+        {expenses.map((expense) => {
+          if (expense.date.getFullYear() == year) {
+            return (
+              <ExpenseItem
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date}
+                key={expense.id}
+              />
+            );
+          }
+        })}
+      </Card>
+    );
+  } else {
+    return (
+      <Card className="expenses">
+        <ExpensesFilter onFilter={filterHandler} />
+        {expenses.map((expense) => {
+          return (
+            <ExpenseItem
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+              key={expense.id}
+            />
+          );
+        })}
+      </Card>
+    );
+  }
 }
 
 export default Expenses;
